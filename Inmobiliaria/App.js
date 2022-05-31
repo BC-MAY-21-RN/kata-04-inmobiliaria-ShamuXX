d92609a651e95e5e77d512a17f63dbcede865bd4
Samuel <samuel16mendoza@gmail.com>
2022-05-31 01:12:35 -0500

    Component named "Item" was created
    This component contains all the information or elements that we need for a real-state like name, address, image, price and others.

---
+++ Inmobiliaria/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Item from './src/components/item';
+
+const App = () => {
+  return (
+    <View>
+      <Item></Item>
+    </View>
+  );
+};
+
+export default App;
